fe2b2330ff5c993086908fd07d20bc9e13536f15
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2018-09-22 17:03:57 -0500

    Se incluyen slider

--- index.php
+++ index.php
@@ -10,36 +10,7 @@
     <title>Monty</title>
 </head>
 <body>
-<header>
-</header>
-<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-  <img src="img/logo-mentha-blanco3.png" alt="monty">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Cocinas</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Muebles</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="#">Acabados</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="#">Blog </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="#">Contacto</a>
-      </li>
-    </ul>
-    
-  </div>
-</nav>
+    <?php include("nav.php"); ?>
+    <?php include("slider.php"); ?>
 </body>
 </html>
---
+++ slider.php
@@ -0,0 +1,21 @@
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="img-fluid" alt="Responsive image" src="img/cocina-mentha-3.JPG" alt="First slide">
+    </div>
+    <div class="carousel-item">
+    <img class="img-fluid" alt="Responsive image" src="img/cocina-mentha2.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+    <img class="img-fluid" alt="Responsive image" src="img/cocina-mentha4.jpg" alt="First slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    
+  </a>
+</div>
