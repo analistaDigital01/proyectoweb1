1eabde17d646f20fff47b8e54f7e1634eb4a765c
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2018-09-22 15:29:45 -0500

    Se incluye archivo jquery y sección nav

--- index.php
+++ index.php
@@ -2,10 +2,44 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="js/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <title>Monty</title>
 </head>
 <body>
+<header>
+</header>
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <img src="img/logo-mentha-blanco3.png" alt="monty">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cocinas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Muebles</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Acabados</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Blog </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Contacto</a>
+      </li>
+    </ul>
     
+  </div>
+</nav>
 </body>
 </html>
