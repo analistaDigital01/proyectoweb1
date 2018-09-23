b01412e99698447f42fe4544c75ca36737c99a52
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2018-09-23 15:20:25 -0500

    se incluye normalize.css y se incluye el nav dentro del header

--- slider.php
+++ slider.php
@@ -10,12 +10,5 @@
     <img class="img-fluid" alt="Responsive image" src="img/cocina-mentha4.jpg" alt="First slide">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    
-  </a>
+ 
 </div>
